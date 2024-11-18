74cda33c392df8f7474f0743c6931b4f8d951e3f
Frank Castillo Ccanto <145064667+Frank-VV@users.noreply.github.com>
2024-11-18 17:42:41 -0500

    ecommerce
    ecommerce1

---
+++ ecommerce/KATSUH/script.js
@@ -0,0 +1,17 @@
+function showSection(sectionId) {
+    // Ocultar todas las secciones
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+
+    // Mostrar la sección seleccionada
+    const activeSection = document.getElementById(sectionId);
+    activeSection.classList.add('active');
+}
+
+// Mostrar la sección de "Inicio" por defecto al cargar la página
+document.addEventListener("DOMContentLoaded", function() {
+    showSection('inicio');
+});
+
